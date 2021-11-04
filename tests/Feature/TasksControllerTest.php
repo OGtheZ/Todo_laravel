f601167fee15ea5bce8d41aa126cd1cb1e822ae8
OGtheZ <zzaicevsedg@gmail.com>
2021-11-04 09:58:35 +0200

    added another test

--- tests/Feature/TasksControllerTest.php
+++ tests/Feature/TasksControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -30,5 +31,15 @@ class TasksControllerTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertViewIs('createTaskForm');
+    }
+
+    public function test_able_to_visit_edit_task_page()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $task = Task::factory()->create();
+
+        $response = $this->get("/tasks/$task->id/edit");
+        $response->assertStatus(200);
     }
 }
